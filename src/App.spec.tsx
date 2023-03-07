2572f7ea36493d2c7d6b1ffaf96a7b7e8704f819
TonySilva7 <tony.deve7oper@gmail.com>
2023-03-06 22:24:24 -0300

    teste 3

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,13 +1,23 @@
 import '@testing-library/jest-dom';
 
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
 import { describe, expect, test } from 'vitest';
 
 import App from './App';
+import { store } from './app/store';
+import { theme01 } from './styles/theme';
 
 describe('App', () => {
   it('renders App component', () => {
-    render(<App />);
+    render(
+      <Provider store={store}>
+        <ThemeProvider theme={theme01}>
+          <App />
+        </ThemeProvider>
+      </Provider>,
+    );
     expect(screen.getByText('Ready to code!')).toBeInTheDocument();
   });
 });
