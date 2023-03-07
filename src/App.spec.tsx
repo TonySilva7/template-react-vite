40f6d906f275245bb15eb3359e0f8fad6025871b
TonySilva7 <tony.deve7oper@gmail.com>
2023-03-07 09:33:27 -0300

    chore: resolve conflict

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -7,7 +7,13 @@ import App from './App';
 
 describe('App', () => {
   it('renders App component', () => {
-    render(<App />);
+    render(
+      <Provider store={store}>
+        <ThemeProvider theme={theme01}>
+          <App />
+        </ThemeProvider>
+      </Provider>,
+    );
     expect(screen.getByText('Ready to code!')).toBeInTheDocument();
   });
 });
